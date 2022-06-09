8c33943d51eff189e0dac2996dae38878f49060b
momo3213 <morgane.hayotte@cpe.fr>
2022-06-09 09:57:58 +0200

    farmer

--- projet.tar/projet/projet/main.py
+++ projet.tar/projet/projet/main.py
@@ -23,7 +23,7 @@ def main():
     tr.translation.y = -np.amin(m.vertices, axis=0)[1]
     tr.translation.z = -5
     tr.rotation_center.z = 0.2
-    texture = glutils.load_texture('stegosaurus.jpg')
+    texture = glutils.load_texture('farmer.jpg')
     o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
     viewer.add_object(o)
 
